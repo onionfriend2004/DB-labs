802a7f0e03d8f53bd623b0af6c9fb872102c94c8
Alexey Lupenkov <alupenkov@inbox.ru>
2024-10-18 00:12:50 +0300

    second var

--- lab1/app.py
+++ lab1/app.py
@@ -12,6 +12,36 @@ with open("./data/dbconfig.json") as f:
 
 provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))
 
+# @app.route("/", methods=['GET', 'POST'])
+# def product_index():
+#     if request.method == "GET":
+#         return render_template("input_category.html")
+#     else:
+#         prod_category = request.form.get('prod_category')
+
+#         # Проверка наличия параметра
+#         if not prod_category:
+#             return "Error: Category parameter is missing"
+
+#         # Проверка, что параметр является целым числом
+#         if not prod_category.isdigit():
+#             return "Error: Category must be an integer"
+
+#         try:
+#             _sql = provider.get('product.sql', prod_category=prod_category)
+#             result, schema = select_list(app.config['db_config'], _sql)
+#             print("res_info.result = ", result)
+#             # Проверка наличия данных в результате
+#             if not result:
+#                 return "No result"
+
+#             prod_title = 'Результаты из БД'
+#             return render_template("dynamic.html", prod_title=prod_title, products=result)
+#         except Exception as e:
+#             # Логирование ошибок
+#             app.logger.error(f"Error executing SQL query: {e}")
+#             return "Error: An error occurred while processing your request"
+        
 @app.route('/', methods=['GET'])
 def product_handler():
     return render_template("input_category.html")
